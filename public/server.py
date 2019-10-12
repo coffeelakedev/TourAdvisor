b04dbebf8465bb946ae7c76c42fa88e2c5b4f89c
Rithvik Nishad <rithviknishad@outlook.com>
2019-10-13 00:35:18 +0530

    commit with you

---
+++ public/server.py
@@ -0,0 +1,15 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.redirect("https://touradvisorapp.web.app/signin.html")
+
+def CreateApp():
+    return tornado.web.Application([(r"/", MainHandler), ])
+
+if __name__ == "__main__":
+    app = CreateApp()
+    app.listen(8080)
+    tornado.ioloop.IOLoop.current().start()
+    

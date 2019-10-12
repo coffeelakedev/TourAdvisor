5b10732970b7a18d6ca25305a96504b287b42f97
Rithvik Nishad <rithviknishad@outlook.com>
2019-10-12 22:29:23 +0530

    commit

--- ui/server.py
+++ ui/server.py
@@ -3,14 +3,12 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.redirect("https://touradvisorapp.web.app/dashboard.html")
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+def CreateApp():
+    return tornado.web.Application([(r"/", MainHandler), ])
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
+    app = CreateApp()
+    app.listen(8080)
     tornado.ioloop.IOLoop.current().start()

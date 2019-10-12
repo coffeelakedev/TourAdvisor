f516b53ebb78ef44be7dde880962edf72b7ad886
AshBlade <jeenamurali100tab@gmail.com>
2019-10-12 17:47:37 +0530

    E

--- ui/FeedPut.js
+++ ui/FeedPut.js
@@ -1,6 +1,7 @@
 function new_plan()
 {
     console.log("Added New plan button detected!");
-    const feed = document.getElementById('Feed');
-    feed.append('')
+    var plan = document.getElementById('modal1');
+    var instance = M.Modal.getInstance(plan);
+    instance.open();
 }

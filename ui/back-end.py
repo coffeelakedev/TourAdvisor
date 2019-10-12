630ecab4c0bf6432d7c032003b326940c89bf0b6
AshBlade <jeenamurali100tab@gmail.com>
2019-10-12 22:26:40 +0530

    E

--- ui/back-end.py
+++ ui/back-end.py
@@ -4,6 +4,7 @@ from firebase import firebase
 
 key="AIzaSyC8cn_tgtIiNErOM5S5pVj8Eu9Y8gUkiXY"
 google_autocomplete = "https://maps.googleapis.com/maps/api/place/autocomplete/json"
+google_detail = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
 db = firebase.FirebaseApplication("https://touradvisorapp.firebaseio.com/", None)
 AUTH = False
 
@@ -29,16 +30,28 @@ def add_plan(destination, budget):
     global google_autocomplete
     if AUTH:
         choice = []
-        google_autocomplete += '?key=' + key + '&input=' + destination.replace(' ', '+')
-        print(google_autocomplete)
-        with urllib.request.urlopen(google_autocomplete) as url:
+        link = google_autocomplete + '?key=' + key + '&input=' + destination.replace(' ', '+')
+        print(link)
+        with urllib.request.urlopen(link) as url:
             data = json.loads(url.read().decode())['predictions']
             # pprint.pprint(data)
             for i in data:
                 for j in i:
                     if j == 'description':
-                        choice.append(i[j])
+                        choice.append((i[j], i['placeid']))
         return choice
     return False
 
+def place_detail(destination):
+    global google_detail
+    if AUTH:
+        link = google_detail + '?key=' + key + '&=fields=name,rating,formatted_phone_number' + '&input=' + destination.replace(' ', '+')
+        print(link)
+        with urllib.request.urlopen(link) as url:
+            data = json.loads(url.read().decode())
+            pprint.pprint(data)
 
+https://maps.googleapis.com/maps/api/place/details/json?
+place_id=ChIJN1t_tDeuEmsRUsoyG83frY4&
+fields=name,rating,formatted_phone_number
+&key=YOUR_API_KEY

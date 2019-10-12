f428ac66ba39b6f357e7685eae212e77f1c23bcb
Rithvik Nishad <rithviknishad@outlook.com>
2019-10-13 03:07:17 +0530

    mycommit

--- public/server.py
+++ public/server.py
@@ -3,13 +3,16 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.redirect("https://touradvisorapp.web.app/signin.html")
+        self.redirect("/signin.html")
+
+class SignInHandler(tornado.web.RequestHandler):
+    def get(self):
+        print(self)
 
 def CreateApp():
-    return tornado.web.Application([(r"/", MainHandler), ])
+    return tornado.web.Application([(r"/", MainHandler), (r"#signin", SignInHandler)])
 
 if __name__ == "__main__":
     app = CreateApp()
-    app.listen(8080)
+    app.listen(8000)
     tornado.ioloop.IOLoop.current().start()
-    
--- ui/server.py
+++ ui/server.py
@@ -3,14 +3,14 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.redirect("/signin.html")
+        self.redirect("signin.html")
 
 class SignInHandler(tornado.web.RequestHandler):
     def get(self):
         print(self)
 
 def CreateApp():
-    return tornado.web.Application([(r"/", MainHandler), (r"#signin", SignInHandler)])
+    return tornado.web.Application([(r"/", MainHandler), (r"signin.html", SignInHandler), ])
 
 if __name__ == "__main__":
     app = CreateApp()

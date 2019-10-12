ec603150706640266d77427a6300ac001a585683
AshBlade <jeenamurali100tab@gmail.com>
2019-10-13 00:53:49 +0530

    E

--- ui/server.py
+++ ui/server.py
@@ -3,13 +3,17 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.redirect("https://touradvisorapp.web.app/signin.html")
+        self.redirect("https://localhost:8000/signin.html")
+
+class SignInHandler(tornado.web.RequestHandler):
+    def get(self):
+        print(self)
 
 def CreateApp():
-    return tornado.web.Application([(r"/", MainHandler), ])
+    return tornado.web.Application([(r"/", MainHandler), (r"#signin", SignInHandler)])
 
 if __name__ == "__main__":
     app = CreateApp()
-    app.listen(8080)
+    app.listen(8000)
     tornado.ioloop.IOLoop.current().start()
     
